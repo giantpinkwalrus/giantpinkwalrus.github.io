6deb61740991a54fc0374f66be90f7c4fa9150dd
Sampo Lamminsalo <sampo.lamminsalo@devnet.fi>
2016-07-15 12:55:15 +0300

    Table stuff

--- script.js
+++ script.js
@@ -14,20 +14,39 @@ var event = {
     friday: {
         srkevo1: {
             '8': 'Street Fighter V',
-            '10': 'Super Smash Bros. Melee',
+            '10': '',
+            '12': 'Super Smash Bros. Melee',
+            '14': '',
             '16': 'Tekken 7 FR Semifinal',
-            '20': 'SSB Melee Pools Round 2'
+            '20': 'SSB Melee Pools Round 2',
+            '22': ''
         },
         srkevo2: {
             '8': 'Super Smash Bros. Wii U',
-            '20': 'SSB Wii U Pools Round 2'
+            '10': '',
+            '12': '',
+            '14': '',
+            '16': '',
+            '20': 'SSB Wii U Pools Round 2',
+            '22': ''
         },
         srkevo3: {
             '8': 'Pokken Tournament',
-            '20': 'SSB Melee Pools Round 2'
+            '10': '',
+            '12': '',
+            '14': '',
+            '16': '',
+            '20': 'SSB Melee Pools Round 2',
+            '22': ''
         },
         capcomfighters: {
-            '8': 'Street Fighter V'
+            '8': 'Street Fighter V',
+            '10': '',
+            '12': '',
+            '14': '',
+            '16': '',
+            '20': '',
+            '22': ''
         }
     },
 }
@@ -38,25 +57,25 @@ function tableCreate(day){
         tbl  = document.createElement('table');
     tbl.style.width  = '100px';
     tbl.style.border = '1px solid black';
-
+    var tHead = tbl.createTHead();
+    var tHeadRow = tHead.insertRow();
+    tHeadRow.insertCell();
+    for(var j = 0; j < timeSlots.length; j++) {
+        var th = tHeadRow.insertCell();
+        th.appendChild(document.createTextNode(friLocalTimes[j].getHours()))
+    }
     for(var stream in day ){
         var tr = tbl.insertRow();
         var streamName = tr.insertCell();
             streamName.appendChild(document.createTextNode(urlStart + stream));
             streamName.style.border = '1px solid black';
         for(var time in day[stream]){
-            if(true){
-                break;
-            } else {
+            var game = day[stream][time];
                 var td = tr.insertCell();
-                td.appendChild(document.createTextNode('Cell'));
+                td.appendChild(document.createTextNode(game));
                 td.style.border = '1px solid black';
-                if(i == 1 && j == 1){
-                    td.setAttribute('rowSpan', '2');
-                }
             }
         }
-    }
     body.appendChild(tbl);
 }
 
